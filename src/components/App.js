d1c0777de4c2604343d04fa9f3bd4c4dc884dcb0
rapackivi <localit1981@gmail.com>
2020-10-24 22:53:51 +0300

    react connected

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import webpackLogo from '../images/webpack-logo.svg'
+
+
+import '../styles/index.scss'; //instead of App.css
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+                <img src={webpackLogo}/>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-// Test import of a JavaScript function
-import {example} from './js/example'
+// // Test import of a JavaScript function
+// import {example} from './js/example'
 
-// Test import of an asset
-import webpackLogo from './images/webpack-logo.svg'
+// // Test import of an asset
+// import webpackLogo from './images/webpack-logo.svg'
 
-// Test import of styles
-import './styles/index.scss'
+// // Test import of styles
+// import './styles/index.scss'
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+// // Appending to the DOM
+// const logo = document.createElement('img')
+// logo.src = webpackLogo
 
-const heading = document.createElement('h1')
-heading.textContent = example()
+// const heading = document.createElement('h1')
+// heading.textContent = example()
 
-const app = document.querySelector('#root')
-app.append(logo, heading)
+// const app = document.querySelector('#root')
+// app.append(logo, heading)
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
